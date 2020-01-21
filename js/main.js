315c9ef932824bd63466ad3ba1e06734966b1430
adam3278 <programista3278@gmail.com>
2020-01-21 15:52:51 +0100

    Upload base files

---
+++ js/main.js
@@ -0,0 +1,3 @@
+function showYt (code) {
+	bootbox.alert("<iframe style='width: 100%; height: 20vh;' src='https://www.youtube-nocookie.com/embed/" + code + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+}
